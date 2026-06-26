18a2e4813a2a81c4e82416926505cd0f5df2f2c7
Juliano Parreira dos Santos <jparreirasantos@gmail.com>
2026-06-26 14:09:54 -0300

    Primeiro commit

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API rodando 🚀");
+});
+
+app.get("/dados", (req, res) => {
+  res.json({ mensagem: "Olá do backend!" });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Servidor rodando");
+});
