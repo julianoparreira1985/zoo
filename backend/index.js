74801bfcf68c5a963795113ceef41f07e7a91809
Juliano Parreira dos Santos <jparreirasantos@gmail.com>
2026-06-26 14:12:53 -0300

    Alteração do arquivo index.js

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("API rodando 🚀");
+  res.send("API rodando 🚀 top, top!");
 });
 
 app.get("/dados", (req, res) => {
