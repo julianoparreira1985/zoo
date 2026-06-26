35b3b2e1ea329e66d82e0c3a354605c765dd150f
Juliano Parreira dos Santos <jparreirasantos@gmail.com>
2026-06-26 14:53:57 -0300

    fix database connection

--- backend/index.js
+++ backend/index.js
@@ -14,16 +14,16 @@ app.get("/dados", (req, res) => {
 });
 
 
+
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
-app.get("/hora", async (req, res) => {
-  const result = await pool.query("SELECT NOW()");
-  res.json(result.rows);
-});
 
 
 
